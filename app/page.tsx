8cff2f0d96730758cf129b3a756e0f6349bcc6cb
Moyo-Made <adegbitemoyomade2004@gmail.com>
2025-02-19 05:15:56 +0100

    Updates / route

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+'use client'
 
-const page = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+
+export default function Home() {
+  const router = useRouter()
+  
+  useEffect(() => {
+    router.push('/signup')
+  }, [])
+  
+  return null
 }
-
-export default page
