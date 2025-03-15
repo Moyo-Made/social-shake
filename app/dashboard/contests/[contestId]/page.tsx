eb0250128d141b39d148520daf56a64bfce0c2da
MoyoMadeIt <mac@MoyoMadeIt.local>
2025-03-15 14:18:32 -0700

    Fix build error

--- app/dashboard/contests/[contestId]/page.tsx
+++ app/dashboard/contests/[contestId]/page.tsx
@@ -1,23 +1,13 @@
-
 import { ContestFormProvider } from "@/components/dashboard/newContest/ContestFormContext";
 import SideNavLayout from "@/components/dashboard/SideNav";
 import ContestDetailPage from "@/components/dashboard/ViewContests/ContestDetails";
 import React from "react";
 
-interface PageProps {
-  params: {
-    contestId: string;
-  }
-}
-
-export default function Page({ params }: PageProps) {
-  // Extract the contestId from params
-  const { contestId } = params;
-  
+export default function Page() {
   return (
     <SideNavLayout>
       <ContestFormProvider>
-        <ContestDetailPage contestId={contestId} />
+        <ContestDetailPage contestId={""} />
       </ContestFormProvider>
     </SideNavLayout>
   );
