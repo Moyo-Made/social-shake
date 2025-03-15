2f69bd9ae41ffd2db0f68da242430f3472a6c538
MoyoMadeIt <mac@MoyoMadeIt.local>
2025-03-15 14:31:43 -0700

    Fix build error

---
+++ components/dashboard/ViewContests/ContestDetailsPage.tsx
@@ -0,0 +1,21 @@
+// import { ContestFormProvider } from "@/components/dashboard/newContest/ContestFormContext";
+// import SideNavLayout from "@/components/dashboard/SideNav";
+// import ContestDetails from "@/components/dashboard/ViewContests/ContestDetails";
+// import React from "react";
+
+// interface PageParams {
+// 	contestId: string;
+//   }
+
+//  const ContestDetailsPage = ({ params }: { params: PageParams }) => {
+// 	const { contestId } = params;
+//   return (
+// 	<SideNavLayout>
+// 	  <ContestFormProvider>
+// 		<ContestDetails contestId={contestId} />
+// 	  </ContestFormProvider>
+// 	</SideNavLayout>
+//   );
+// }
+
+// export default ContestDetailsPage;
