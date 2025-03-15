bfa2109f719832d84083444da735d523d62d3a93
MoyoMadeIt <mac@MoyoMadeIt.local>
2025-03-15 13:56:58 -0700

    Fix build error

--- app/dashboard/contests/[contestId]/page.tsx
+++ app/dashboard/contests/[contestId]/page.tsx
@@ -3,14 +3,12 @@ import SideNavLayout from "@/components/dashboard/SideNav";
 import ContestDetailPage from "@/components/dashboard/ViewContests/ContestDetails";
 import React from "react";
 
-const page = (params: { contestId: string }) => {
-	return (
-	  <SideNavLayout>
-		<ContestFormProvider>
-		  <ContestDetailPage contestId={params.contestId} />
-		</ContestFormProvider>
-	  </SideNavLayout>
-	);
-  };
-
-export default page;
+export default function Page({ params }: { params: { contestId: string } }) {
+  return (
+    <SideNavLayout>
+      <ContestFormProvider>
+        <ContestDetailPage contestId={params.contestId} />
+      </ContestFormProvider>
+    </SideNavLayout>
+  );
+}
