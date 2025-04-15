018c297c99ec9048226d68c3579bd4442c67019d
Moyo-Made <adegbitemoyomade2004@gmail.com>
2025-04-14 18:53:16 -0700

    fix: refactor layout structure by removing deprecated layout file and adding new layout for manage users

--- components/admin/AdminLogin.tsx
+++ components/admin/AdminLogin.tsx
@@ -50,7 +50,7 @@ export default function AdminLogin() {
 
   return (
     <div className="flex items-center justify-center min-h-screen w-full">
-      <div className="w-full max-w-2xl p-8 space-y-8 bg-white rounded-lg shadow-md">
+      <div className="w-full max-w-xl p-8 space-y-8 bg-white rounded-lg shadow-md">
         <div>
           <h2 className="mt-6 text-3xl font-extrabold text-center text-gray-900">
             Admin Login
